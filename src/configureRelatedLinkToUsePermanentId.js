e82fb42e2c02385566b8ea9e048d854bac6be72a
eva <eva.de.haas@liones.nl>
2019-06-28 11:07:07 +0200

    Upgrade to SDK 7.7

--- src/configureRelatedLinkToUsePermanentId.js
+++ src/configureRelatedLinkToUsePermanentId.js
@@ -1,64 +1,42 @@
-define([
-	'fontoxml-dita/configureAsRelatedLink',
-	'fontoxml-families/createElementMenuButtonWidget',
-	'fontoxml-families/createMarkupLabelWidget'
-], function (
-	configureAsRelatedLink,
-	createElementMenuButtonWidget,
-	createMarkupLabelWidget
-) {
-	'use strict';
+import configureAsRelatedLink from 'fontoxml-dita/src/configureAsRelatedLink.js';
+import createElementMenuButtonWidget from 'fontoxml-families/src/createElementMenuButtonWidget.js';
+import createMarkupLabelWidget from 'fontoxml-families/src/createMarkupLabelWidget.js';
 
-	return function configureXrefToUsePermanentId (sxModule) {
-		// To enable permanentId's in references (also known as the reference pipeline), change
-		// hasPermanentId to TRUE, and set popoverData.targetIsPermanent to TRUE.
+export default function configureXrefToUsePermanentId(sxModule) {
+	// To enable permanentId's in references (also known as the reference pipeline), change
+	// hasPermanentId to TRUE, and set popoverData.targetIsPermanent to TRUE.
 
-		// A cross link. (cross link is used by default)
-		configureAsRelatedLink(sxModule, 'self::link', undefined, {
-			contextualOperations: [
-				{ name: ':link-insert-linktext' },
-				{ name: ':link-insert-desc' }
-			],
-			backgroundColor: 'grey',
-			defaultTextContainer: 'linktext',
-			hasPermanentId: true,
-			popoverComponentName: 'DitaCrossReferencePopover',
-			popoverData: {
-				editOperationName: ':contextual-edit-link[@format=dita]',
-				targetIsPermanentId: true,
-				targetQuery: '@href'
-			},
-			showWhen: 'always',
-			blockHeaderLeft: [
-				createMarkupLabelWidget()
-			],
-			blockOutsideAfter: [
-				createElementMenuButtonWidget()
-			]
-		});
+	// A cross link. (cross link is used by default)
+	configureAsRelatedLink(sxModule, 'self::link', undefined, {
+		contextualOperations: [{ name: ':link-insert-linktext' }, { name: ':link-insert-desc' }],
+		backgroundColor: 'grey',
+		defaultTextContainer: 'linktext',
+		hasPermanentId: true,
+		popoverComponentName: 'DitaCrossReferencePopover',
+		popoverData: {
+			editOperationName: ':contextual-edit-link[@format=dita]',
+			targetIsPermanentId: true,
+			targetQuery: '@href'
+		},
+		showWhen: 'always',
+		blockHeaderLeft: [createMarkupLabelWidget()],
+		blockOutsideAfter: [createElementMenuButtonWidget()]
+	});
 
-		// A web link.
-		configureAsRelatedLink(sxModule, 'self::link[@format="html"]', undefined, {
-			contextualOperations: [
-				{ name: ':link-insert-linktext' },
-				{ name: ':link-insert-desc' }
-			],
-			backgroundColor: 'grey',
-			defaultTextContainer: 'linktext',
-			hasPermanentId: true,
-			popoverComponentName: 'WebReferencePopover',
-			popoverData: {
-				editOperationName: ':contextual-edit-link[@format=html]',
-				targetIsPermanentId: true,
-				targetQuery: '@href'
-			},
-			showWhen: 'always',
-			blockHeaderLeft: [
-				createMarkupLabelWidget()
-			],
-			blockOutsideAfter: [
-				createElementMenuButtonWidget()
-			]
-		});
-	};
-});
+	// A web link.
+	configureAsRelatedLink(sxModule, 'self::link[@format="html"]', undefined, {
+		contextualOperations: [{ name: ':link-insert-linktext' }, { name: ':link-insert-desc' }],
+		backgroundColor: 'grey',
+		defaultTextContainer: 'linktext',
+		hasPermanentId: true,
+		popoverComponentName: 'WebReferencePopover',
+		popoverData: {
+			editOperationName: ':contextual-edit-link[@format=html]',
+			targetIsPermanentId: true,
+			targetQuery: '@href'
+		},
+		showWhen: 'always',
+		blockHeaderLeft: [createMarkupLabelWidget()],
+		blockOutsideAfter: [createElementMenuButtonWidget()]
+	});
+}
